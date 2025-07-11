88f232cdb2bc177f843f434ea91e018137633030
IrfanSahruli <irfansahruli244@gmail.com>
2025-07-12 00:28:50 +0700

    tes

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
-const database = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-    host: process.env.DB_HOST,
+const database = new Sequelize(process.env.DBNAME, process.env.nama, process.env.PASSWORD, {
+    host: process.env.HOST,
     dialect: 'mysql'
 });
 
